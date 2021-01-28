bc5f27c0094e7075fe6535554c379bb426fedcfd
burnsred94 <67732974+burnsred94@users.noreply.github.com>
2021-01-28 14:51:34 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+    if ((arr == null)
+        || (arr.length < 1)
+        || isNaN(arr.reduce((a, b) => a + b)))
+        return 0;
+    let minValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        minValue = arr[i] < minValue ? arr[i] : minValue;
+    }
+    return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if ((arr == null)
+        || (arr.length < 1)
+        || isNaN(arr.reduce((a, b) => a + b)))
+        return 0;
+    let maxValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        maxValue = arr[i] > maxValue ? arr[i] : maxValue;
+    }
+    return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(arr) {
+    if ((arr == null)
+        || (arr.length < 1)
+        || isNaN(arr.reduce((a, b) => a + b)))
+        return 0;
+    let avgValue = (arr.reduce((a, b) => a + b)) / arr.length;
+    return avgValue;
+}	
